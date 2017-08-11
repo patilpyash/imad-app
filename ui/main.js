104f7057ac9a1d9ff261f9c2fe5d794db441e5c5
patilpyash <patilpyash@gmail.com>
2017-08-11 17:41:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
-var element = document.getElementById(text);
+var element = document.getElementById('text');
 element.innerHTML("New Text");
-var img = document.getElementById(madi);
+var img = document.getElementById('madi');
 function moveRight(){
     var marginLeft = 10;
     marginLeft = marginLeft + 'px';
