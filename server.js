6b0c1bc47e90b6851e2ab9b8de6b61396c5643a5
patilpyash <patilpyash@gmail.com>
2017-08-29 13:15:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 var counter = 0;
-app.get("/counter", function(req,res){
+app.get('/counter' , function(req,res){
    counter=counter+1;
    res.send(counter.toString());
 });
